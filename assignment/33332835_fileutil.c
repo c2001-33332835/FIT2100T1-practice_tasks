f8800aa7594d8dbaf9c69025ee1d9d68b5ee9299
Frankium <tis7bfrankie@gmail.com>
2022-08-22 04:11:19 +1000

    c is giving me a stroke

--- assignment/33332835_fileutil.c
+++ assignment/33332835_fileutil.c
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 #define STDIN 0
 #define STDOUT 1
@@ -13,63 +14,128 @@
 
 #define PROGRAM_NAME "fileutil"
 
-// naming convensions: snake_case, "fd" prefix when it's file descriptor. Capitalisation for struct name
+// naming convensions: snake_case for globals/locals/statics and functions. CapitalizedName for struct names.
 
+// Impelementation of Linked List: LinkedNode is the nodes of the list.
 typedef struct {
-    int* files;
-    int file_count;
-    void** pointers;
-    int pointer_count;
-} CleanupData;
-
-typedef struct {
-    void* data;
-    LinkedNode* next;
-    int is_end;
+    void* data; // the actual element of the list
+    struct LinkedNode* next; // points to the next element of the list
+    int is_end; // if the node is at the end of a list
+    int is_null; // if the node is null (happens when the list is newly created and no value is provided)
 } LinkedNode;
 
-LinkedNode* linked_list_get_last_node(LinkedNode* source){
-    LinkedNode* node = source;
-    while (!(node->is_end)){
-        node = node->next;
-    }
-    return node;
+void initialize_new_node(LinkedNode* node){
+    // initialization of a new node as a new list
+    // PARAM:
+    //   - node: takes in a newly defined node, and initialize required values
+    // RETURN:
+    //   void.
+
+    node->is_end = true;
+    node->is_null = true;
 }
 
-LinkedNode* linked_list_append(LinkedNode* source, void* data){
-    LinkedNode* last_node = linked_list_get_last_node(source);
-    LinkedNode* new_node;
-    new_node->data = data;
-    new_node->is_end = true;
-    last_node->next = new_node;
-    last_node->is_end = false;
-    return new_node;
+void initialize_new_append_node(LinkedNode* node){
+    // initialization of a new node to be appended to another node
+    // PARAM:
+    //   - node: takes in a newly defined node, and initialize required values
+    // RETURN:
+    //   void.
+
+    node->is_end = true;
+    node->is_null = false;
 }
 
-int linked_list_length(LinkedNode* source){
-    LinkedNode* node = source;
-    int length = 1;
-    while (!(node->is_end)){
-        node = node->next;
-        length++;
+LinkedNode* linked_list_get_last_node(LinkedNode* start){
+    // Gets the last node of a given starting node. 
+    // PARAM:
+    //   - start: a reference of the starting node to search
+    // RETURN:
+    //   - the reference of the last node. Returns the starting node if the list is still null, or has one element only.
+
+    if (start->is_null) {
+        return start;
     }
-    return length;
+
+    LinkedNode* result = start;
+    while (!(result->is_end)){
+        result = result->next;
+    }
+    return result;
 }
+
+void linked_list_append_node(LinkedNode* start, LinkedNode* new, void* value){
+    // Append a value to the linked list to the ned of the list or updates the given node if the list is null.
+    // PARAM:
+    //   - start: takes in any node from the list
+    //   - new: the address of the new node to be appended
+    //   - value the value to be added to the new node
+    // RETURN:
+    //   - void.
+
+    LinkedNode* target;
+    if (start->is_null){
+        target = start;
+        printf("1");
+    }
+    else {
+        target = &new;
+        linked_list_get_last_node(start)->is_end = false;
+        linked_list_get_last_node(start)->next = target;
+        printf("2");
+    }
+    initialize_new_append_node(target);
+    target->data = value;
+    return target;
+}
+
+
+int linked_list_length(LinkedNode* start){
+    // Finds out the length between the starting point and the end of the list.
+    // PARAM:
+    //   - start: a reference of any node in the list (give the absolute starting node of the list if the length of the whole list is needed).
+    // RETURN:
+    //   - the length between the starting node and the ending node.
+    if (start->is_null) {
+        return 0;
+    }
+
+    int count = 1;
+    LinkedNode* result = start;
+    while (!(result->is_end)){
+        result = result->next;
+        count ++;
+    }
+    return count;
+}
+
+// A structure to store the cleanup information including a list of file descriptors and a list of pointers.
+typedef struct {
+    LinkedNode* files; // files to be closed
+    LinkedNode* pointers; // pointers to be free
+} CleanupData;
+
 
 void cleanup(CleanupData* cleanup_data){
     // close all files and free all pointers.
+    // PARAM:
+    //   - cleanup_data: a reference of the CleanupData for this program.
+    // RETURN:
+    //   - void.
 
-    int* file_pointer = cleanup_data->files
-
-    for (int i = 0; i < cleanup_data->file_count; i++){
-        close(* file_pointer);
-        file_pointer++;
+    int files_len = linked_list_length(cleanup_data->files);
+    LinkedNode* files_node = cleanup_data->files;
+    for (int i = 0; i < files_len; i++){
+        int* file_desc = (int*) files_node->data;
+        close(*file_desc);
+        files_node = files_node->next;
     }
 
-    void** pointer_pointer = cleanup_data->pointers; // pointer to pointers to be freed
-    for (int i = 0; i < cleanup_data->pointer_count; i++){
-        free(*pointer_pointer);
-        pointer_pointer++;
+    int pointers_len = linked_list_length(cleanup_data->pointers);
+    LinkedNode* pointers_node = cleanup_data->pointers;
+    for (int i = 0; i < pointers_len; i++){
+        free(pointers_node->data);
+        pointers_node = pointers_node->next;
     }
 }
 
@@ -99,14 +165,21 @@ void initialize_file(int* file_fd, int file_mode, char* filename, int permission
 }
 
 int main(int argc, char* argv[]){
+    printf("a");
     // ===== PROGRAM PREPARE STAGE
     // define constants
     int source_file_mode = O_RDONLY;
     int destination_file_mode = O_WRONLY | O_CREAT;
     int destination_file_permission = 0664;
+
+    // define cleanup struct. files and pointers in this struct will be closed and freed automatically by cleanup()
     CleanupData cleanup_data;
-    cleanup_data.file_count = 0;
-    cleanup_data.pointer_count = 0;
+    LinkedNode files_descriptors;
+    LinkedNode heap_pointers;
+    initialize_new_node(&files_descriptors);
+    initialize_new_node(&heap_pointers);
+    cleanup_data.files = &files_descriptors;
+    cleanup_data.pointers = &heap_pointers;
     
     // define fallback values for when they are not given through the cli argments.
     char fallback_source_filename[] = "sample.txt";
@@ -114,20 +187,23 @@ int main(int argc, char* argv[]){
 
     // read and parse argv
     int append_flag = false;
-    char destination_filename[] = "";
+    char destination_filename[] = "1";
     char source_filename[strlen(fallback_source_filename)];
     strcpy(source_filename, fallback_source_filename);
     int word_count = fallback_word_count;
 
     // prepare and open source file
     int fd_source_file;
-    initialize_file(&fd_source_file, source_file_mode, source_filename, &cleanup_data);
-    cleanup_data.pointer_count
+    initialize_file(&fd_source_file, source_file_mode, source_filename, 0, &cleanup_data);
+    LinkedNode fd_source_file_node;
+    linked_list_append_node(&files_descriptors, &fd_source_file_node, &fd_source_file);
 
     // prepare and open destination file if applicable
-    if (append_flag){
-        int fd_destination_file;
+    int fd_destination_file;
+    LinkedNode fd_destination_file_node;
+    if (!append_flag){
         initialize_file(&fd_destination_file, destination_file_mode, destination_filename, destination_file_permission, &cleanup_data);
+        linked_list_append_node(&files_descriptors, &fd_destination_file_node, &fd_destination_file);
     }
 
     return 0;
