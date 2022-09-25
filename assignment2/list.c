f2b0c3b318c6d6bb53ea4ac687b10bb39f6bad61
Frankium <tis7bfrankie@gmail.com>
2022-09-25 22:37:05 +1000

    add support for empty list

--- assignment2/list.c
+++ assignment2/list.c
@@ -19,10 +19,18 @@ linked_node_t* list_get_start_node(linked_node_t* node){
 
 linked_node_t* list_append_node(linked_node_t* node, void* data){
     linked_node_t* end = list_get_last_node(node);
+
+    if (end->empty){
+        end->content = data;
+        end->empty = 0;
+        return end;
+    }
+
     linked_node_t* new_node = (linked_node_t*) malloc(sizeof(linked_node_t));
     new_node->end = 1;
     new_node->prev = end;
     new_node->content = data;
+    new_node->empty = 0;
     end->next = new_node;
     end->end = 0;
     return new_node;
@@ -32,6 +40,7 @@ linked_node_t* list_create(void* data){
     linked_node_t* new_node = (linked_node_t*) malloc(sizeof(linked_node_t));
     new_node->end = 1;
     new_node->start = 1;
+    new_node->empty = 0;
     new_node->content = data;
     return new_node;
 }
@@ -41,10 +50,21 @@ linked_node_t* list_create_size(int size){
     new_node->content = NULL;
     new_node->start = 1;
     new_node->end = 1;
+    new_node->empty = 0;
     
     for (int i = 1; i < size; i ++){
         list_append_node(new_node, NULL);
     }
+    
+    return new_node;
+}
+
+linked_node_t* list_create_empty(int size){
+    linked_node_t* new_node = (linked_node_t*) malloc(sizeof(linked_node_t));
+    new_node->content = NULL;
+    new_node->start = 1;
+    new_node->end = 1;
+    new_node->empty = 1;
     
     return new_node;
 }
@@ -67,7 +87,12 @@ void list_remove_last(linked_node_t* node){
 }
 
 void list_set_item(linked_node_t* node, int index, void* data){
-    list_get_node(node, index)->content = data;
+    linked_node_t* target = list_get_node(node, index);
+    if (target->empty){
+        return;
+    }
+    
+    target->content = data;
 }
 
 linked_node_t* list_get_node(linked_node_t* node, int index){
@@ -84,8 +109,12 @@ void* list_get_item(linked_node_t* node, int index){
 }
 
 unsigned list_length(linked_node_t* node){
-    unsigned len;
     linked_node_t* curr = list_get_start_node(node);
+    if (curr->empty){
+        return 0;
+    }
+    
+    unsigned len;
     for (len = 1; !curr->end; len++){
         curr = curr->next;
     }
--- assignment2/list.h
+++ assignment2/list.h
@@ -7,14 +7,15 @@ typedef struct linked_node {
     void* content;
     int end;
     int start;
+    int empty;
 } linked_node_t;
-
 
 linked_node_t* list_get_last_node(linked_node_t* node);
 linked_node_t* list_get_start_node(linked_node_t* node);
 linked_node_t* list_append_node(linked_node_t* node, void* data);
 linked_node_t* list_create(void* data);
 linked_node_t* list_create_size(int size);
+linked_node_t* list_create_empty();
 void list_free(linked_node_t* node);
 void list_remove_last(linked_node_t* node);
 void list_set_item(linked_node_t* node, int index, void* data);
