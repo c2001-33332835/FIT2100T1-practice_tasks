a7393e728cf1826445dd4d738477b4d2aee9dd00
frankium <frankium>
2022-09-28 17:19:34 +1000

    fix interface for print record

--- assignment2/process_record.h
+++ assignment2/process_record.h
@@ -16,6 +16,6 @@ typedef enum {
 linked_node_t* pr_read_source_file(int fd);
 process_record_t* pr_parse_record_line(char* line);
 void pr_sort_records(linked_node_t* records, process_record_field_t sort_by, int asc);
-char* pr_record_to_str(process_record_t* record);
+void* pr_print_record(process_record_t* record);
 
 #endif
